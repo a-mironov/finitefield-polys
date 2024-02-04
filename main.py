d543c87687286ccf6b78b3ca28619c9e52b89197
a-mironov <43029603+a-mironov@users.noreply.github.com>
2024-02-04 22:50:57 +0200

    polynomial data-mgmt logic
    implemented commands:
    - setchar: set field characteristic, flush polys if changed
    - char: view current characteristic
    - create: create a new polynomial
    - update: update an existing polynomial with a new value
    - delete: delete an existing polynomial
    - show: display an existing polynomial
    - showall: display all stored polynomials
    removed echo command

--- main.py
+++ main.py
@@ -1,40 +1,120 @@
+import polynomial
+
 exitflag = False
+# warning flag for setchar
+charflag = False
 
 welcomemsg = "Welcome to Finite Field Polynomial Calculator!"
 print(welcomemsg)
 
+# command help messages
 cmds_list = {}
-cmds_list["echo"] = """Auxiliary command.
-Prints all of its arguments on the screen."""
-cmds_list["exit"] = "Exits the program."
+cmds_list["exit"] = """Usage: exit\n
+Exits the program."""
 cmds_list["help"] = """Usage: help <cmd>\n
 Prints a description of its input command. Ignores subsequent arguments."""
+cmds_list["create"] = """Usage: create <name> <coeffs>\n
+Creates a polynomial by name `name` with coefficients `coeffs`, read in ascending order of degree."""
+cmds_list["show"] = """Usage: show <name>\n
+Displays the polynomial by name `name` (if it exists) on the screen."""
+cmds_list["showall"] = """Usage: showall\n
+Displays all currently stored polynomials on the screen."""
+cmds_list["delete"] = """Usage: delete <name>\n
+Deletes the polynomial by name `name`."""
+cmds_list["update"] = """Usage: update <name> <coeffs>\n
+Updates the polynomial by name `name` with new coefficients <coeffs>."""
+cmds_list["setchar"] = """Usage: setchar <number>\n
+Sets the base field characteristic to `number`. If this causes a change,
+all stored polynomials are deleted! Warns the user before proceeding."""
+cmds_list["char"] = """Usage: char\n
+Displays the current characteristic of the base field."""
 
 
+cmd = ""
 while not exitflag:
+    # if last command was not setchar, clear the flag
+    if cmd != "setchar":
+        charflag = False
     ## input processing
     # raw user input
     userin = input("> ")
+    
     # clean up duplicate spaces in user input
+    # join and re-split...
     userin_clean = " ".join(userin.rstrip(" ").split())
     args = userin_clean.split(" ")
     cmd = args[0]
-    argc = len(args)-1
+    argc = len(args) - 1
+    # all data-mgmt commands have name as their first argument
+    if argc > 0:
+        name = args[1] 
 
     ## commands processing
-    ## eventually all logic should be redirected to modules
-    ## and from here they will only be called
-    # self-explanatory
-    if cmd == "exit":
-        print("Exiting...")
-        exitflag = True
-        continue
-    # debug command, to be removed in final ver
-    elif cmd == "echo":
-        for i in range(1, argc+1):
-            print(f"Argument {i}: {args[i]}")
-        continue
-    # no command recognized
-    else:
-        print(f"Unknown command: {cmd}!")
-        continue
+    match cmd:
+        case "setchar":
+            if not charflag:
+                print(f"""Warning! Changing the characteristic will delete all
+stored polynomials! Repeat the command to confirm.""")
+                charflag = True
+                continue
+            else:
+                try:
+                    new_char = int(args[1])
+                except ValueError:
+                    print(f"Could not parse {args[1]} as an integer!")
+                else:
+                    try:
+                        polynomial.set_characteristic(new_char)
+                    except ValueError as e:
+                        print(e)
+                    else:
+                        print(f"Characteristic set to {new_char} successfully.")
+        case "char":
+            print(f"Field characteristic is {polynomial.FCH}.")
+        case "exit":
+            print("Goodbye.")
+            exitflag = True
+
+        case "help":
+            # if user typed only "help", return desc for help itself
+            if argc == 0:
+                print(cmds_list["help"])
+            elif args[1] in cmds_list.keys():
+                print(cmds_list[args[1]])
+            else:
+                print(f"Unrecognized command: {args[1]}!")
+        case "create":
+            try:
+                polynomial.make_poly(name, args[2:])
+            except ValueError as e:
+                print(e)
+            else:
+                print(f"Polynomial {name} created.")
+        case "show":
+            try:
+                display_string = polynomial.display_poly(name)
+            except KeyError as e:
+                print(e)
+            else:
+                print(display_string)
+        case "showall":
+            if len(polynomial.poly_dict.keys()) == 0:
+                print("No polynomials currently stored.")
+            else:
+                print(polynomial.display_all())
+        case "delete":
+            try:
+                polynomial.del_poly(name)
+            except KeyError as e:
+                print(e)
+            else:
+                print(f"Polynomial {name} deleted.")
+        case "update":
+            try:
+                polynomial.update_poly(name, args[2:])
+            except KeyError as e:
+                print(e)
+            else:
+                print(f"Polynomial {name} updated.")
+        case _:
+            print(f"Unknown command: {cmd}!")
--- polynomial.py
+++ polynomial.py
@@ -3,15 +3,146 @@
 # contains logic for finite field calculations
 # and polynomial management (input, output, creation, replacement and deletion)
 
-# field characteristic
-global FCH = 7
+from math import sqrt, floor
 
+# field characteristic
+FCH = 7
+# polynomial dictionary
+poly_dict = {}
+
+# polynomials stored as lists of coefficients
+# in ascending order, so p.coeffs[i] == x^i coefficient
 class Poly():
-    def __init__(self, cfs):
+    def __init__(self, cfs: list):
         self.coeffs = cfs
         self.normalize()
 
-    # normalize coefficients to range 0..FCH-1
+    def __str__(self):
+        # will make something more sophisticated here...
+        return str(self.coeffs)
+
     def normalize(self):
-        self.coeffs = map(lambda n: n % FCH, self.coeffs)
-        
+        # normalize coefficients to range 0..FCH-1
+        self.coeffs = list(map(lambda n: n % FCH, self.coeffs))
+        # trim zeroes
+        zeroidx = len(self.coeffs)-1
+        while zeroidx > 0 and self.coeffs[zeroidx] == 0:
+            zeroidx -= 1
+        del self.coeffs[zeroidx+1:]
+
+def is_prime(n: int):
+    """
+    Checks if its input is prime. Auxiliary.
+
+    n: int -- The number whose primality is to be checked.
+    """
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    limit = floor(sqrt(n)) + 1
+    # check for divisibility by odd numbers between 3 and sqrt(n)
+    for d in range(3, limit, 2):
+        if n % d == 0:
+            return False
+    return True
+
+def make_poly(name: str, coefficients: list):
+    """
+    makes a new polynomial and adds it to the dict
+
+    name: str - the name of the polynomial
+    coefficients: list - the coefficients of the polynomial
+    """
+    # possible exceptions: 2
+    # 1. bad name: supplied name is already found in the dict
+    
+    if name in poly_dict.keys():
+        raise ValueError(f"Name {name} already in use! Use `update` to overwrite polynomials.")
+    
+    #2. bad coeffs: at least one entry in cfs can't parse as an int
+    cfs_clean = [0] * len(coefficients)
+    for i in range(len(coefficients)):
+        try:
+            cfs_clean[i] = int(coefficients[i])
+        except ValueError:
+            raise ValueError(f"""Coefficient on x^{i}
+                            read as "{coefficients[i]}", cannot parse!""")
+
+    #if we made it here, the name is good and the coefficients list is clean
+    poly_dict[name] = Poly(cfs_clean)
+
+def del_poly(name: str):
+    """
+    deletes a polynomial from the dict
+
+    name: str - the name of the polynomial 
+    """
+    # possible exceptions: 1
+    # 1. bad name: polynomial by that name doesn't exist in poly_dict anyway
+    if name not in poly_dict.keys():
+        raise KeyError(f"No polynomial with name {name} to delete.")
+
+    # if we made it here, the name is good and deletable
+    poly_dict.pop(name)
+
+def display_poly(name: str):
+    """
+    outputs a string representation of the given polynomial
+
+    name: str - the name of the polynomial
+    """
+    try:
+        poly = poly_dict[name]
+    except KeyError:
+        raise KeyError(f"No polynomial by name {name} to print.")
+    return name + " = " + str(poly)
+
+def display_all():
+    """
+    outputs a string representation for the entire polynomial dict
+    """
+    display_string = ""
+    for name in poly_dict.keys():
+        display_string += display_poly(name) + "\n"
+
+    return display_string
+
+def update_poly(name: str, coefficients: list):
+    """
+    updates a polynomial with new coefficients
+    the value of the old polynomial is discarded
+
+    name: str - the name of the polynomial
+    coefficients: list - the new coefficients of the polynomial
+    """
+    if name not in poly_dict.keys():
+        raise KeyError(f"""No polynomial with name {name} to update!
+Use `create` to create new polynomials.""")
+
+    del_poly(name)
+    make_poly(name, coefficients)
+
+def set_characteristic(new_char: int):
+    """
+    Resets the characteristic of the base field.
+    Unless the old value matches the new, flushes all stored polynomials.
+
+    new_char: int -- the new value of the characteristic
+    """
+    global FCH
+    # if new characteristic is the same as the old, do nothing
+    if new_char == FCH:
+        return
+    elif new_char <= 1:
+        raise ValueError(f"Invalid characteristic value {new_char} -- Not a natural number.")
+    elif not is_prime(new_char):
+        raise ValueError(f"Invalid characteristic value {new_char} -- Not prime.")
+    else:
+        # new characteristic is prime and differs from old
+        FCH = new_char
+        # flushing the polynomials
+        for name in poly_dict.keys():
+            del_poly(name)
