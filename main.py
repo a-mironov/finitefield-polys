f908c5064777f83865713d7dd6089bb8af635982
a-mironov <43029603+a-mironov@users.noreply.github.com>
2024-02-05 13:00:54 +0200

    basic arithmetic
    Arithmetic:
    - addition, subraction, multiplication
    Meta:
    - `list` command

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ charflag = False
 welcomemsg = "Welcome to Finite Field Polynomial Calculator!"
 print(welcomemsg)
 
-# command help messages
+## command help messages
 cmds_list = {}
+# meta commands
 cmds_list["exit"] = """Usage: exit\n
 Exits the program."""
 cmds_list["help"] = """Usage: help <cmd>\n
 Prints a description of its input command. Ignores subsequent arguments."""
+cmds_list["list"] = """Usage: list\n
+Lists all currently supported commands."""
+# initialization commands
+cmds_list["setchar"] = """Usage: setchar <number>\n
+Sets the base field characteristic to `number`. If this causes a change,
+all stored polynomials are deleted! Warns the user before proceeding."""
+cmds_list["char"] = """Usage: char\n
+Displays the current characteristic of the base field."""
+# data management commands
 cmds_list["create"] = """Usage: create <name> <coeffs>\n
 Creates a polynomial by name `name` with coefficients `coeffs`, read in ascending order of degree."""
 cmds_list["show"] = """Usage: show <name>\n
@@ -23,11 +33,13 @@ cmds_list["delete"] = """Usage: delete <name>\n
 Deletes the polynomial by name `name`."""
 cmds_list["update"] = """Usage: update <name> <coeffs>\n
 Updates the polynomial by name `name` with new coefficients <coeffs>."""
-cmds_list["setchar"] = """Usage: setchar <number>\n
-Sets the base field characteristic to `number`. If this causes a change,
-all stored polynomials are deleted! Warns the user before proceeding."""
-cmds_list["char"] = """Usage: char\n
-Displays the current characteristic of the base field."""
+# arithmetic commands
+cmds_list["add"] = """Usage: add <name1> <name2> <result>\n
+Adds the polynomials by names `name1` and `name2` and stores the result in `result`. `name1` and `name2` must exist, while `result` must not exist."""
+cmds_list["subtract"] = """Usage: subtract <name1> <name2> <result>\n
+Subtracts the polynomials by names `name1` and `name2` and stores the result in `result`. `name1` and `name2` must exist, while `result` must not exist."""
+cmds_list["multiply"] = """Usage: multiply <name1> <name2> <result>\n
+Multiplies the polynomials by names `name1` and `name2` and stores the result in `result`. `name1` and `name2` must exist, while `result` must not exist."""
 
 
 cmd = ""
@@ -69,6 +81,7 @@ stored polynomials! Repeat the command to confirm.""")
                         print(e)
                     else:
                         print(f"Characteristic set to {new_char} successfully.")
+                        charflag = False
         case "char":
             print(f"Field characteristic is {polynomial.FCH}.")
         case "exit":
@@ -83,6 +96,10 @@ stored polynomials! Repeat the command to confirm.""")
                 print(cmds_list[args[1]])
             else:
                 print(f"Unrecognized command: {args[1]}!")
+        case "list":
+            print("Available commands:")
+            for command in cmds_list.keys():
+                print(command)
         case "create":
             try:
                 polynomial.make_poly(name, args[2:])
@@ -94,7 +111,8 @@ stored polynomials! Repeat the command to confirm.""")
             try:
                 display_string = polynomial.display_poly(name)
             except KeyError as e:
-                print(e)
+                name = e.args[0]
+                print(f"No polynomial by name {name} to print.")
             else:
                 print(display_string)
         case "showall":
@@ -106,15 +124,48 @@ stored polynomials! Repeat the command to confirm.""")
             try:
                 polynomial.del_poly(name)
             except KeyError as e:
-                print(e)
+                name = e.args[0]
+                print(f"No polynomial by name {name} to delete.")
             else:
                 print(f"Polynomial {name} deleted.")
         case "update":
             try:
                 polynomial.update_poly(name, args[2:])
             except KeyError as e:
-                print(e)
+                name = e.args[0]
+                print(f"No polynomial by name {name} to update! "+
+                      "Use `create` to create new polynomials.")
             else:
                 print(f"Polynomial {name} updated.")
+        case "add":
+            try:
+                polynomial.addmake_poly(args[1], args[2], args[3])
+            except ValueError as e:
+                print(e)
+            except KeyError as e:
+                name = e.args[0]
+                print(f"Polynomial {name} not found!")
+            else:
+                print(f"Sum {args[1]}+{args[2]} stored in {args[3]}.")
+        case "subtract":
+            try:
+                polynomial.submake_poly(args[1], args[2], args[3])
+            except ValueError as e:
+                print(e)
+            except KeyError as e:
+                name = e.args[0]
+                print(f"Polynomial {name} not found!")
+            else:
+                print(f"Difference {args[1]}-{args[2]} stored in {args[3]}.")
+        case "multiply":
+            try:
+                polynomial.mulmake_poly(args[1], args[2], args[3])
+            except ValueError as e:
+                print(e)
+            except KeyError as e:
+                name = e.args[0]
+                print(f"Polynomial {name} not found!")
+            else:
+                print(f"Product {args[1]}*{args[2]} stored in {args[3]}.") 
         case _:
             print(f"Unknown command: {cmd}!")
--- polynomial.py
+++ polynomial.py
@@ -1,34 +1,119 @@
 # polynomial module
 
-# contains logic for finite field calculations
-# and polynomial management (input, output, creation, replacement and deletion)
+# contains logic for polynomial management
+# (creation, replacement, deletion, display)
 
 from math import sqrt, floor
+from enum import Flag, auto
 
 # field characteristic
 FCH = 7
 # polynomial dictionary
 poly_dict = {}
+# inverse lookup
+
+# WIP!!
+class DisplayFlag(Flag):
+    # true => print terms in descending order, false => ascending order
+    DESCENDING = auto()
+    # not yet implemented: balanced -- coeffs in -(FCH-1)/2 .. (FCH-1)/2
+    # instead of 0 .. FCH-1
+    # BALANCED = auto()
 
 # polynomials stored as lists of coefficients
 # in ascending order, so p.coeffs[i] == x^i coefficient
 class Poly():
     def __init__(self, cfs: list):
         self.coeffs = cfs
+        # empty list => zero polynomial
+        if len(self.coeffs) == 0:
+            self.coeffs = [0]
         self.normalize()
 
     def __str__(self):
-        # will make something more sophisticated here...
-        return str(self.coeffs)
+        # constant => print as-is
+        if len(self.coeffs) == 1:
+            return str(self.coeffs[0])
+        # otherwise iterate thru terms
+        # adding only those with nonzero coeffs to the string rep
+        terms = []
+        for i in range(len(self.coeffs)):
+            coefficient = self.coeffs[i]
+            if coefficient != 0:
+                # constant term printed as is
+                if i == 0:
+                    terms.append(str(self.coeffs[0]))
+                else:
+                    # power x^i, printed as just x for i=1
+                    power = f"x^{i}"
+                    if i == 1:
+                        power = "x"
+                    if coefficient == 1:
+                        terms.append(power)
+                    else:
+                        terms.append(str(coefficient) + power)
+                    
+        return " + ".join(terms)
 
     def normalize(self):
-        # normalize coefficients to range 0..FCH-1
+        """
+        normalizes all coefficients to range 0..FCH-1
+        and trims leading zeroes
+        """
         self.coeffs = list(map(lambda n: n % FCH, self.coeffs))
         # trim zeroes
         zeroidx = len(self.coeffs)-1
         while zeroidx > 0 and self.coeffs[zeroidx] == 0:
             zeroidx -= 1
         del self.coeffs[zeroidx+1:]
+
+    def __add__(self, other):
+        """
+        adds two polynomials
+        """
+        total = Poly([0])
+        total.coeffs = [0] * max(len(self.coeffs),len(other.coeffs))
+        n = len(total.coeffs)
+        for i in range(n):
+            if i < len(self.coeffs):
+                total.coeffs[i] += self.coeffs[i]
+            if i < len(other.coeffs):
+                total.coeffs[i] += other.coeffs[i]
+        total.normalize()
+        return total
+
+    def __sub__(self, other):
+        """
+        subtracts two polynomials (self - other)
+        """
+        total = Poly([0])
+        total.coeffs = [0] * max(len(self.coeffs),len(other.coeffs))
+        n = len(total.coeffs)
+        for i in range(n):
+            if i < len(self.coeffs):
+                total.coeffs[i] += self.coeffs[i]
+            if i < len(other.coeffs):
+                total.coeffs[i] -= other.coeffs[i]
+        total.normalize()
+        return total
+
+    def __mul__(self, other):
+        """
+        multiplies two polynomials
+        """
+        total = Poly([0])
+        # polynomial degrees; assumes normalized
+        m = len(self.coeffs) - 1
+        n = len(other.coeffs) - 1
+        total.coeffs = [0]*(m+n+1)
+        # naive convolution
+        for k in range(m+n+1):
+            for i in range(k+1):
+                j = k - i
+                if i <= m and j <= n:
+                    total.coeffs[k] += (self.coeffs[i]*other.coeffs[j]) % FCH
+        total.normalize()
+        return total
 
 def is_prime(n: int):
     """
@@ -49,6 +134,7 @@ def is_prime(n: int):
             return False
     return True
 
+
 def make_poly(name: str, coefficients: list):
     """
     makes a new polynomial and adds it to the dict
@@ -68,11 +154,13 @@ def make_poly(name: str, coefficients: list):
         try:
             cfs_clean[i] = int(coefficients[i])
         except ValueError:
-            raise ValueError(f"""Coefficient on x^{i}
-                            read as "{coefficients[i]}", cannot parse!""")
+            raise ValueError(f"Coefficient on x^{i} "+
+                             f"read as \"{coefficients[i]}\", "+
+                             "cannot parse!")
 
     #if we made it here, the name is good and the coefficients list is clean
     poly_dict[name] = Poly(cfs_clean)
+
 
 def del_poly(name: str):
     """
@@ -83,10 +171,11 @@ def del_poly(name: str):
     # possible exceptions: 1
     # 1. bad name: polynomial by that name doesn't exist in poly_dict anyway
     if name not in poly_dict.keys():
-        raise KeyError(f"No polynomial with name {name} to delete.")
+        raise KeyError(name)
 
     # if we made it here, the name is good and deletable
     poly_dict.pop(name)
+
 
 def display_poly(name: str):
     """
@@ -97,8 +186,9 @@ def display_poly(name: str):
     try:
         poly = poly_dict[name]
     except KeyError:
-        raise KeyError(f"No polynomial by name {name} to print.")
+        raise KeyError(name)
     return name + " = " + str(poly)
+
 
 def display_all():
     """
@@ -110,6 +200,7 @@ def display_all():
 
     return display_string
 
+
 def update_poly(name: str, coefficients: list):
     """
     updates a polynomial with new coefficients
@@ -119,11 +210,83 @@ def update_poly(name: str, coefficients: list):
     coefficients: list - the new coefficients of the polynomial
     """
     if name not in poly_dict.keys():
-        raise KeyError(f"""No polynomial with name {name} to update!
-Use `create` to create new polynomials.""")
+        raise KeyError(name)
 
     del_poly(name)
     make_poly(name, coefficients)
+
+
+def addmake_poly(name1: str, name2: str, result: str):
+    """
+    pulls two polynomials from poly_dict, adds them, and stores the result
+    name1 and name2 must exist, while result must NOT exist
+
+    name1: str -- name of the first polynomial to be summed
+    name2: str -- name of the second polynomial to be summed
+    result: str -- name under which to store the result
+    """
+    # read polynomials from dict
+    try:
+        addend1 = poly_dict[name1]
+        addend2 = poly_dict[name2]
+    except KeyError as e:
+        raise e
+    # ensure name `result` does not exist
+    if result in poly_dict.keys():
+        raise ValueError(f"Cannot store addition result -- name {result}"+
+                         "already in use.")
+    # now we have our polys and can add them
+    total = addend1 + addend2
+    # not calling make_poly b/c the polynomial object is already there
+    poly_dict[result] = total
+
+def submake_poly(name1: str, name2: str, result: str):
+    """
+    pulls two polynomials from poly_dict, subtracts them, and stores the result
+    name1 and name2 must exist, while result must NOT exist
+
+    name1: str -- name of the first polynomial to be summed
+    name2: str -- name of the second polynomial to be summed
+    result: str -- name under which to store the result
+    """
+    # read polynomials from dict
+    try:
+        addend1 = poly_dict[name1]
+        addend2 = poly_dict[name2]
+    except KeyError as e:
+        raise e
+    # ensure name `result` does not exist
+    if result in poly_dict.keys():
+        raise ValueError(f"Cannot store subtraction result -- name {result}"+
+                         "already in use.")
+    # now we have our polys and can subtract them
+    total = addend1 - addend2
+    # not calling make_poly b/c the polynomial object is already there
+    poly_dict[result] = total
+
+def mulmake_poly(name1: str, name2: str, result: str):
+    """
+    pulls two polynomials from poly_dict, multiplies them, and stores the result
+    name1 and name2 must exist, while result must NOT exist
+
+    name1: str -- name of the first polynomial to be summed
+    name2: str -- name of the second polynomial to be summed
+    result: str -- name under which to store the result
+    """
+    # read polynomials from dict
+    try:
+        addend1 = poly_dict[name1]
+        addend2 = poly_dict[name2]
+    except KeyError as e:
+        raise e
+    # ensure name `result` does not exist
+    if result in poly_dict.keys():
+        raise ValueError(f"Cannot store addition result -- name {result}"+
+                         "already in use.")
+    # now we have our polys and can add them
+    total = addend1 * addend2
+    # not calling make_poly b/c the polynomial object is already there
+    poly_dict[result] = total
 
 def set_characteristic(new_char: int):
     """
@@ -137,12 +300,13 @@ def set_characteristic(new_char: int):
     if new_char == FCH:
         return
     elif new_char <= 1:
-        raise ValueError(f"Invalid characteristic value {new_char} -- Not a natural number.")
+        raise ValueError(f"Invalid characteristic value {new_char} -- "+
+                         "Not a natural number.")
     elif not is_prime(new_char):
-        raise ValueError(f"Invalid characteristic value {new_char} -- Not prime.")
+        raise ValueError(f"Invalid characteristic value {new_char} -- "+
+                         "Not prime.")
     else:
         # new characteristic is prime and differs from old
         FCH = new_char
         # flushing the polynomials
-        for name in poly_dict.keys():
-            del_poly(name)
+        poly_dict.clear()
